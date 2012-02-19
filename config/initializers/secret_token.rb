1b4850f6b7929b9fddd361b7fb50b636522dfadd
Smabbly <smabbly>
2012-02-19 12:54:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9090dbb45fc6a1da31f39c1fcedf2ed3cdd610fe72a8ff613d1b192338731565e45419a83d2f84b9b018fd80f211bec4f211e749613b946944c68e5c99530835'
